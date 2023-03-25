bfc5f7725e5c3bafc087d88c66867bf404cc8462
K Smaran Sai <97632455+KSmaranSai@users.noreply.github.com>
2023-03-25 13:56:07 +0530

    Bug Fixes
    1.  Click working when game not started or between interval of level cleared resulting 'Wrong answer '
    2.  Touch to start added for mobile and tablets

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
+//declaration of all variables
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
 let level = 0;
 let currentCount = 0;
-let started=0;
+let started = 0;
 
 //function to play sound
 function playSound(item) {
@@ -11,6 +12,7 @@ function playSound(item) {
     audio.play();
 }
 
+//function to generate next colour
 function nextSequence() {
     //generate random number
     let random_colour = Math.floor(Math.random() * 4);
@@ -29,54 +31,132 @@ function nextSequence() {
     level++;
 
     //change head to level number
-    $("h1").text("Level " + level);
+    $(".head").text("Level " + level);
 
     //initialise the current count after every sequence
     currentCount = 0;
 }
 
-//for every button colour
-buttonColours.forEach(function (item) {
+//function to add vent click if game is started else keep it off
+function clickOnOff() {
+    //for every button colour
+    buttonColours.forEach(function (item) {
+        if (started) {
+            //for click of button
+            $("." + item).click(function () {
+                $("." + item).addClass('flash');
+                setTimeout(function () { $("." + item).removeClass('flash'); }, 100);
+                playSound(item);
+                userClickedPattern.push(item);
 
-    //for click of button
-    $("." + item).click(function () {
-        $("." + item).addClass('flash');
-        setTimeout(function () { $("." + item).removeClass('flash'); }, 100);
-        playSound(item);
-        userClickedPattern.push(item);
-
-        //check user input
-        checkAnswer(currentCount++);
+                //check user input
+                checkAnswer(currentCount++);
+            })
+        }
+        else{
+            //remove click function
+            $('.'+item).off('click');
+        }
     })
-})
+}
 
+//check answer after ever input
 function checkAnswer(currentLevel) {
-    console.log(userClickedPattern);
-    console.log(gamePattern);
+
+    //If at certain index user entered is not equal to gamepattern
     if (userClickedPattern[currentLevel] != gamePattern[currentLevel]) {
+
+        //play wrong audio
         let audio = new Audio("sounds/wrong.mp3")
         audio.play();
+
+        //adds class game over to body
         $("body").addClass('game-over');
-        setTimeout(function(){
+
+        //removes class game over from body after 200ms
+        setTimeout(function () {
             $("body").removeClass('game-over');
-        },200)
+        }, 200)
+
+        //display option to restart
         $(".head").text("Game Over, Press Any Key to Restart");
+
+        //sets all the variable parameters to the start value
         startOver();
     }
-    else if(userClickedPattern.length===gamePattern.length){
-        $("h1").text("Level "+level+" Cleared !!");
-        setTimeout(function(){nextSequence();},2000);
+    //if all the elements entered are same jump to next level
+    else if (userClickedPattern.length === gamePattern.length) {
+
+        //level cleared message
+        $(".head").text("Level " + level + " Cleared !!");
+        
+        //paused state
+        started=0;
+
+        //turn off click function of buttons between the interval after level is cleared
+        clickOnOff();
+
+        // call nextSequence after 2000ms
+        setTimeout(function () {
+
+            //next sequence called 
+            nextSequence();
+
+            //resume state
+            started=1;
+            
+            //turn on click
+            clickOnOff();
+        }, 2000);
     }
 }
 
-function startOver(){
-    level=0;
-    gamePattern=[];
-    userClickedPattern=[];
-    started=0;
+//function to set parameters to default value
+function startOver() {
+
+    //restarts level
+    level = 0;
+    
+    //empty all the pattern values
+    gamePattern = [];
+    userClickedPattern = [];
+
+    //game ended
+    started = 0;
+
+    //turn off click function of buttons
+    clickOnOff();
 }
 
-$("body").keypress(function (event) {if(!started){
-    nextSequence();
-    started=1;
-}});
+//if a keypress is encountered the flowing code will run (for pc)
+$("body").keypress(function (event) {
+
+    if (!started) {
+        
+        //generate first colour if game not started (for pc)
+        nextSequence();
+
+        //game started
+        started = 1;
+
+        //Switch on the click function of buttons
+        clickOnOff();
+    }
+
+});
+
+//if a touch is encountered the flowing code will run (for moblies or tablets or any touch screen device)
+$("body").on({
+
+    //if touch encountered the function will run
+    'touchstart': function () {
+        if (!started) {
+
+            //generate first colour if game not started (for moblies or tablets or any touch screen device)
+            nextSequence();
+
+            //game started
+            started = 1;
+        }
+    }
+});
